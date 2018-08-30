60d180c777785da897b69d88922db581d302a878
ManuelCaetano93FG <manuelcaetano93@gmail.com>
2018-08-30 18:51:01 -0500

    Added initial inputs for first requirements, as well as base component generation.

---
+++ src/app/calendar-generator/calendar-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-generator',
+  templateUrl: './calendar-generator.component.html',
+  styleUrls: ['./calendar-generator.component.css']
+})
+export class CalendarGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
